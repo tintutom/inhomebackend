694f2454adc24e8550b5c4d2728b8fb46398000c
Tintu Tom <tintutom2000@gmail.com>
2023-12-10 17:38:00 +0530

    add whitenoise

--- inhome_doctor_consulting/urls.py
+++ inhome_doctor_consulting/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path("chat/", include("chat.urls")),
 ] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
 
 
 
