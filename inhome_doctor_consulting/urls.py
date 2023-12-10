30575f1b59c4a67a6ea2133790b8b5e8477b4072
Tintu Tom <tintutom2000@gmail.com>
2023-12-11 01:10:52 +0530

    rearrange settings

--- inhome_doctor_consulting/urls.py
+++ inhome_doctor_consulting/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path('doctors/',include('doctors.urls')),
     path('cadmin/',include('cadmin.urls')),
     path("chat/", include("chat.urls")),
-] + static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
 
 
 
