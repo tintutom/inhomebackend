57a889c9dfc2b90a722105bfcc23c410a63917e8
Tintu Tom <tintutom2000@gmail.com>
2023-12-10 12:10:25 +0530

    setup aws RDS to the django project

--- inhome_doctor_consulting/settings.py
+++ inhome_doctor_consulting/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from decouple import config
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -72,16 +71,15 @@ CHANNEL_LAYERS = {
     },
 }
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST")
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': '5432',
     }
 }
 
